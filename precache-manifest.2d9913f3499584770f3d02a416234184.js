04e87cb3c0f059a990c367efedbcb1a5a9aec64a
mingmonth <ys9922@naver.com>
2019-04-06 23:32:34 +0900

    Updates

---
+++ precache-manifest.2d9913f3499584770f3d02a416234184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "5cc5ab022d4eadb05813",
+    "url": "/movie_app/static/js/main.34f2203f.chunk.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie_app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "5cc5ab022d4eadb05813",
+    "url": "/movie_app/static/css/main.9bb5336a.chunk.css"
+  },
+  {
+    "revision": "661288cc5e7fa37df52f31f350664aa9",
+    "url": "/movie_app/index.html"
+  }
+];
